e90fcd91b573058a86b5fdb9e32891bad587dbe9
Simon Gareste <simon@gareste.fr>
2016-10-24 22:13:37 +0200

    Fix js libraries and call order

--- website/static/website/js/format_identifier.js
+++ website/static/website/js/format_identifier.js
@@ -1,23 +1,14 @@
-window.DATA = window.DATA || {}, window.DATA.importResponsiveUtilities = function() {
-    "use strict";
+window.WEBSITE_LIB = window.WEBSITE_LIB || {}, window.WEBSITE_LIB.importResponsiveUtilities = function() { "use strict";
     var e = 767,
         i = 1024,
         t = function() {
-            return window.matchMedia("(max-device-width:" + e + "px)").matches
-        },
+            return window.matchMedia("(max-device-width:" + e + "px)").matches },
         n = function() {
-            return window.matchMedia("(max-device-width:" + i + "px)").matches
-        },
+            return window.matchMedia("(max-device-width:" + i + "px)").matches },
         r = {};
     return r.mobile = function() {
-        return t()
-    }, r.tablet = function() {
-        return n()
-    }, r.device = function() {
-        return n() || t()
-    }, r.mobileLandscape = function() {
-        return t() && window.matchMedia("(orientation: landscape)").matches
-    }, r.fullHeightModeOrDevice = function() {
-        return "Full Browser Height" === window.DATA.theme.gallery_image_height || this.device()
-    }, r
-};
+        return t() }, r.tablet = function() {
+        return n() }, r.device = function() {
+        return n() || t() }, r.mobileLandscape = function() {
+        return t() && window.matchMedia("(orientation: landscape)").matches }, r.fullHeightModeOrDevice = function() {
+        return "Full Browser Height" === window.WEBSITE_CONF.theme.gallery_image_height || this.device() }, r };
